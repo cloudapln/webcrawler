c69847a8c9dd449f29a62e061a452be63f591e32
vingaike <vingaike@gmail.com>
2017-01-22 12:57:38 +0000

    get next url to be crawled

--- src/main/java/com/example/WebCrawler.java
+++ src/main/java/com/example/WebCrawler.java
@@ -7,16 +7,26 @@ import static com.example.UrlChecker.domainName;
 import static com.example.UrlChecker.isInternalUrl;
 
 /**
- * Main class to crawling for given url
+ * Main class to crawling for given url and cascade links within pages
  */
 public class WebCrawler {
     private List<String> pagesToVisit = new LinkedList<String>();
-    
+    private Set<String> pagesVisited = new HashSet<String>();
+
+
     public Map<String, List<String>> search(String searchUrl) throws MalformedURLException {
         String domainName = domainName(searchUrl);
         // Map to store links found on respective page
         Map<String, List<String>> pageLinksMap = new HashMap<>();
+        String currentUrl;
+
         PageParser pageParser = new PageParser();
+        if (this.pagesToVisit.isEmpty()) {
+            currentUrl = searchUrl;
+            this.pagesVisited.add(searchUrl);
+        } else {
+            currentUrl = this.urlToBeCrawled();
+        }
         if (isInternalUrl(searchUrl, domainName)) {
             //parse page, get links list
             List<String> linksOnPage = pageParser.parse(searchUrl);
@@ -27,5 +37,18 @@ public class WebCrawler {
         return pageLinksMap;
     }
 
+    /**
+     * This method returns the next urlToBeCrawled
+     *
+     * @return nextUrlToBeCrawled
+     */
+    private String urlToBeCrawled() {
+        String nextUrlToBeCrawled;
+        do {
+            nextUrlToBeCrawled = this.pagesToVisit.remove(0);
+        } while (this.pagesVisited.contains(nextUrlToBeCrawled));
+        this.pagesVisited.add(nextUrlToBeCrawled);
+        return nextUrlToBeCrawled;
+    }
 
 }
