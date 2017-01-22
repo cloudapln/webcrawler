324b0d742add8c682caa1b87bf5ca53134ef76ac
vingaike <vingaike@gmail.com>
2017-01-22 12:04:48 +0000

    project structure

---
+++ src/main/java/com/example/WebCrawler.java
@@ -0,0 +1,9 @@
+package com.example;
+
+/**
+ * Main class to crawling for given url
+ */
+public class WebCrawler {
+    public void search(String searchUrl) {
+    }
+}
---
+++ src/test/java/com/example/WebCrawlerTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Test;
+
+/**
+ */
+public class WebCrawlerTest {
+    @Test
+    public void search() throws Exception {
+        WebCrawler webCrawler = new WebCrawler();
+        webCrawler.search(" http://wiprodigital.com");
+    }
+}
