518d18f235bca5a7d1b87f2160b4e73f5b5908ee
vingaike <vingaike@gmail.com>
2017-01-22 12:31:09 +0000

    use page parser for web crawling

--- src/main/java/com/example/WebCrawler.java
+++ src/main/java/com/example/WebCrawler.java
@@ -1,9 +1,23 @@
 package com.example;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Main class to crawling for given url
  */
 public class WebCrawler {
-    public void search(String searchUrl) {
+
+    // Map to store links found on respective page
+
+    public Map<String, List<String>> search(String searchUrl) {
+        Map<String, List<String>> pageLinksMap = new HashMap<>();
+        PageParser pageParser = new PageParser();
+        //parse page, get links list
+        List<String> linksOnPage = pageParser.parse(searchUrl);
+        //add links list to map
+        pageLinksMap.put(searchUrl, linksOnPage);
+        return pageLinksMap;
     }
 }
--- src/test/java/com/example/WebCrawlerTest.java
+++ src/test/java/com/example/WebCrawlerTest.java
@@ -2,12 +2,19 @@ package com.example;
 
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertThat;
+
 /**
  */
 public class WebCrawlerTest {
     @Test
     public void search() throws Exception {
         WebCrawler webCrawler = new WebCrawler();
-        webCrawler.search("http://wiprodigital.com");
+        Map<String, List<String>> pageLinksMap = webCrawler.search("http://wiprodigital.com");
+        assertThat(pageLinksMap.get("http://wiprodigital.com").size(), not(0));
     }
 }
